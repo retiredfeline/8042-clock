bae9fcbd85a9ae65dfc5a58404c54f6dcc7d1a3b
Ken Yap <retiredfeline@outlook.com>
2023-01-25 06:34:05 +1100

    Add shift register ternary option using 2 LEDs per trit

---
+++ gentertab.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Generate lookup table for ternary numbers 0 to 59 """
+
+MASKS = [0b00, 0b01, 0b10]
+
+
+def toternary(i):
+    """ Convert i to ternary using 2 bits per trit """
+    result = 0
+    for shift in range(0, 4):
+        rem = i % 3
+        i = i // 3
+        result |= (MASKS[rem] << (shift * 2))
+    return result
+
+
+def main():
+    """ Main loop """
+    for i in range(0, 60):
+        value = toternary(i)
+        print(f"0x{value:02x}")
+
+
+if __name__ == '__main__':
+    main()
